ecdbed9bcde8b26d2d31d3b925c1bbd387e56233
G9 Aerospace <g9militants@gmail.com>
2023-11-13 14:51:44 +0530

    bot.js now runs newsUpdater.js on startup.. yay

--- bot.js
+++ bot.js
@@ -2,14 +2,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const axios = require('axios');
 const fs = require('fs');
+const { spawn } = require('child_process');
 require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
-const apiKey = process.env.NEWS_API_KEY;
-const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
 let updateChannels = {};
 
@@ -24,90 +22,25 @@ function loadChannelsFromFile() {
     }
 }
 
-function saveChannelsToFile(channels) {
-    const data = JSON.stringify(channels, null, 2);
-    fs.writeFileSync('./channels.json', data, 'utf8');
-}
-
-function fetchNews() {
-    return axios.get(newsApiUrl)
-        .then(response => response.data.articles)
-        .catch(error => {
-            console.error('Error fetching news:', error.message);
-            return [];
-        });
-}
-
-function compareNews(oldNews, newNews) {
-    const updates = [];
-
-    // Create a map of old news for efficient lookup
-    const oldNewsMap = new Map(oldNews.map(article => [article.title, true]));
-
-    // Check each new article
-    for (const newArticle of newNews) {
-        // If the title of the new article is not in the old news map, consider it an update
-        if (!oldNewsMap.has(newArticle.title)) {
-            updates.push(newArticle);
-        }
-    }
-
-    return updates;
-}
-
-function loadOldNewsFromFile() {
-    try {
-        const data = fs.readFileSync('./oldNews.json', 'utf8');
-        return JSON.parse(data);
-    } catch (error) {
-        console.warn('Error loading old news from file:', error.message);
-        return [];
-    }
-}
-
-function saveOldNewsToFile(news) {
-    const data = JSON.stringify(news, null, 2);
-    fs.writeFileSync('./oldNews.json', data, 'utf8');
-}
-
-async function sendNewsUpdates() {
-    try {
-        const newNews = await fetchNews();
-
-        // Load old news from a file
-        const oldNews = loadOldNewsFromFile();
-
-        // Compare old news with new news
-        const updates = compareNews(oldNews, newNews);
-
-        // For demonstration purposes, log the updates
-        console.log('News Updates:', updates);
-
-        // Save the new news as old news for the next comparison
-        saveOldNewsToFile(newNews);
-
-        for (const guildId in updateChannels) {
-            const channelId = updateChannels[guildId];
-            const channel = await client.channels.fetch(channelId);
-
-            if (channel && channel.isText()) {
-                for (const article of updates) {
-                    const message = `${article.title}\n${article.url}`;
-                    await channel.send(message);
-                }
-            }
-        }
-    } catch (error) {
-        console.error('Error sending news updates:', error.message);
-    }
-}
-
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
     updateChannels = loadChannelsFromFile();
 
-    // Fetch and send news updates every 30 minutes
-    setInterval(sendNewsUpdates, 30 * 60 * 1000);
+    // Run newsUpdater.js on startup
+    const newsUpdaterProcess = spawn('node', ['newsUpdater.js']);
+
+    // Listen for events from the child process
+    newsUpdaterProcess.stdout.on('data', data => {
+        console.log(`newsUpdater.js output: ${data}`);
+    });
+
+    newsUpdaterProcess.stderr.on('data', data => {
+        console.error(`newsUpdater.js error: ${data}`);
+    });
+
+    newsUpdaterProcess.on('close', code => {
+        console.log(`newsUpdater.js exited with code ${code}`);
+    });
 });
 
 client.on('interactionCreate', require('./handlers/interactionHandler'));
